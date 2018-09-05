a420359aa1efd1a214d300a40983b71bd6c5d18c
Hugo Holmqvist <hugo.holm@live.com>
2018-09-05 16:58:47 +0300

    More const conversions

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const R = require('ramda')
 const request = require('request')
 const translations = require('./translations')
 
-var EVENTS_FILE = 'events.json'
+const EVENTS_FILE = 'events.json'
 
 const WEATHER_URL = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22helsinki%22)%20and%20u=%27c%27&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
 
