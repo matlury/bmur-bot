1f9284d15e4560db8d5b251d1c29976d6db49cf7
Hugo Holmqvist <hugo.holm@live.com>
2018-09-05 17:33:33 +0300

    Fix image tag name

--- scripts/deploy_container.sh
+++ scripts/deploy_container.sh
@@ -2,5 +2,5 @@
 echo "Pushing container image to registry.tko-aly.fi"
 echo $DEPLOY_PASSWORD | docker login registry.tko-aly.fi -u $DEPLOY_USERNAME --password-stdin
 docker build . -t eventbird-tg
-docker tag user-service:latest registry.tko-aly.fi/eventbird-tg:latest
+docker tag eventbird-tg:latest registry.tko-aly.fi/eventbird-tg:latest
 docker push registry.tko-aly.fi/eventbird-tg:latest
