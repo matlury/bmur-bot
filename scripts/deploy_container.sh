bd90e6079141c8278b9c94772750aebd45ee7da8
Hugo Holmqvist <hugo.holm@live.com>
2018-09-06 12:40:35 +0300

    Fix deploy scripts

--- scripts/deploy_container.sh
+++ scripts/deploy_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Pushing container image to registry.tko-aly.fi"
-echo $DEPLOY_PASSWORD | docker login registry.tko-aly.fi -u $DEPLOY_USERNAME --password-stdin
+docker login registry.tko-aly.fi -username=$DEPLOY_USERNAME --password=$DEPLOY_PASSWORD
 docker build . -t eventbird-tg
 docker tag eventbird-tg:latest registry.tko-aly.fi/eventbird-tg:latest
 docker push registry.tko-aly.fi/eventbird-tg:latest
