73dd97edb3aee2ab4cf54e31d7feca1def475ab8
Hugo Holmqvist <hugo.holm@live.com>
2018-09-06 12:43:51 +0300

    Remove = from deploy script docker login

--- scripts/deploy_container.sh
+++ scripts/deploy_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Pushing container image to registry.tko-aly.fi"
-docker login registry.tko-aly.fi -username=$DEPLOY_USERNAME --password=$DEPLOY_PASSWORD
+docker login registry.tko-aly.fi --username $DEPLOY_USERNAME --password $DEPLOY_PASSWORD
 docker build . -t eventbird-tg
 docker tag eventbird-tg:latest registry.tko-aly.fi/eventbird-tg:latest
 docker push registry.tko-aly.fi/eventbird-tg:latest
