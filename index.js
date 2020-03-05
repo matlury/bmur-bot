e2429bf391c22ecbc73acb1369f806eec295e36c
Hugo Holmqvist <hugo.holm@live.com>
2020-03-05 13:14:01 +0200

    Silent dotenv failure

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config({ silent: true })
 const TelegramBot = require('node-telegram-bot-api')
 const fs = require('fs')
 const moment = require('moment')
